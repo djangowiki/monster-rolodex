9d3d40852264c6cb74d44644ffc0a361b5e8fda2
Ikenna Gabriel Ifenna <13273983+djangowiki@users.noreply.github.com>
2020-09-13 16:57:46 +0100

    Updates

---
+++ precache-manifest.f0292dfbc3d6798a4a24a8b5a944e2f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6e4a830250ba68949b24098caebe091",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1c5ffee04febbc368aaa",
+    "url": "/monster-rolodex/static/css/main.75d0ab19.chunk.css"
+  },
+  {
+    "revision": "ef9a8546710f3f1bacbb",
+    "url": "/monster-rolodex/static/js/2.ebd67535.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.ebd67535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5ffee04febbc368aaa",
+    "url": "/monster-rolodex/static/js/main.eb0731ed.chunk.js"
+  },
+  {
+    "revision": "9ae98c422beccd51da85",
+    "url": "/monster-rolodex/static/js/runtime-main.7b16e713.js"
+  }
+]);
